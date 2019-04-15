bebf0e0e54d7308bf74077957803a2a8e298d3a8
Koki Goshima <gosh.namnam0705@gmail.com>
2019-04-15 10:41:57 +0800

    omikuji10:43

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,15 +5,14 @@
 let result = document.getElementById("result");
 let btn = document.getElementById("btn");
 let destinies = ["大凶",　"凶",　"小吉",　"吉",　"中吉",　"大吉"]
-let omikuji;
 
-omikuji = function(arrays){
+function omikuji(){
 	let num = Math.floor(Math.random() * destinies.length);
-	result.innerHTML = arrays[num];
+	result.innerHTML = destinies[num];
 	// result.innerHTML = "hello";
 }
 
-btn.addEventListener("click", omikuji(destinies));
+btn.addEventListener("click", omikuji);
 
 
 
